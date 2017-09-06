8daa5da4bdef06e9aeb84bf65cab4e520a53b8b7
Esvethlynna Pangelinan and Steve Zaske <esvethlynna+steve@>
2017-09-06 15:40:30 -0700

    add initial code for fragment dialog

---
+++ app/src/main/java/com/epicodus/fragment/MainActivity.java
@@ -0,0 +1,37 @@
+package com.epicodus.fragment;
+
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.moodButton) Button mMoodButton;
+    @Bind(R.id.textView) TextView mAppNameTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mMoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+        });
+
+    }
+
+
+}
